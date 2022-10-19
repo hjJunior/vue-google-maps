0eb64080fc5afea9de8e1533f477f1fd4732ab9a
Helio S. Junior <hj_junior@hotmail.com>
2022-10-18 21:22:06 -0300

    Prevent building

--- src/components/autocomplete.vue
+++ src/components/autocomplete.vue
@@ -40,38 +40,44 @@ const props = {
 
 export default {
   mounted() {
-    this.$gmapApiPromiseLazy().then(() => {
-      if (this.selectFirstOnEnter) {
-        downArrowSimulator(this.$refs.input)
-      }
-
-      if (typeof google.maps.places.Autocomplete !== 'function') {
-        throw new Error(
-          "google.maps.places.Autocomplete is undefined. Did you add 'places' to libraries when loading Google Maps?"
-        )
-      }
-
-      /* eslint-disable no-unused-vars */
-      const finalOptions = {
-        ...getPropsValues(this, mappedProps),
-        ...this.options,
-      }
-
-      this.$autocomplete = new google.maps.places.Autocomplete(this.$refs.input, finalOptions)
-      bindProps(this, this.$autocomplete, mappedProps)
-
-      this.$watch('componentRestrictions', (v) => {
-        if (v !== undefined) {
-          this.$autocomplete.setComponentRestrictions(v)
+    if (this.$slots.default || this.$scopedSlots.default) return;
+    this.build();
+  },
+  methods: {
+    build() {
+      this.$gmapApiPromiseLazy().then(() => {
+        if (this.selectFirstOnEnter) {
+          downArrowSimulator(this.$refs.input)
         }
-      })
 
-      // Not using `bindEvents` because we also want
-      // to return the result of `getPlace()`
-      this.$autocomplete.addListener('place_changed', () => {
-        this.$emit('place_changed', this.$autocomplete.getPlace())
+        if (typeof google.maps.places.Autocomplete !== 'function') {
+          throw new Error(
+            "google.maps.places.Autocomplete is undefined. Did you add 'places' to libraries when loading Google Maps?"
+          )
+        }
+
+        /* eslint-disable no-unused-vars */
+        const finalOptions = {
+          ...getPropsValues(this, mappedProps),
+          ...this.options,
+        }
+
+        this.$autocomplete = new google.maps.places.Autocomplete(this.$refs.input, finalOptions)
+        bindProps(this, this.$autocomplete, mappedProps)
+
+        this.$watch('componentRestrictions', (v) => {
+          if (v !== undefined) {
+            this.$autocomplete.setComponentRestrictions(v)
+          }
+        })
+
+        // Not using `bindEvents` because we also want
+        // to return the result of `getPlace()`
+        this.$autocomplete.addListener('place_changed', () => {
+          this.$emit('place_changed', this.$autocomplete.getPlace())
+        })
       })
-    })
+    }
   },
   props: {
     ...mappedPropsToVueProps(mappedProps),
